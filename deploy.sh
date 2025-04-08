92b57a3f9f38f9185e22a499655aa3fd53d4f322
salemango <blackfootedcat2021@gmail.com>
2025-04-08 18:08:41 -0400

    Deploy to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,30 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-set  -e
+# Exit script immediately if a command exits with a non-zero status.
+set -e
 
+# Build the Vue application for production.
 npm run build
 
+# Navigate into the distribution directory.
 cd dist
 
-git init 
-git add -A
-git commit -m 'New Deployment'
-git push -f 'git@github.com:salemango/character-trait-builder.git master: gh-pages'
+# If the repository is a user/organization page
+# git init
+# git remote add origin https://github.com/<USERNAME>/<USERNAME>.github.io
+# git checkout -b main
 
-cd -
+# If the repository is a project page
+git init
+git remote add origin git@github.com:salemango/character-trait-builder.git
+git checkout -b gh-pages
+
+# Copy the contents of the distribution directory to the root of the Git repository
+cp -r * ../
+
+cd ../
+
+# Add all changes, commit, and push to the remote repository.
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push origin gh-pages -f
