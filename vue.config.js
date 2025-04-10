59b0ece90d6a0bc069b76bef16d2cf0fbd3ff487
salemango <blackfootedcat2021@gmail.com>
2025-04-10 11:09:57 -0400

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/character-traits/'
+  publicPath: '/character-traits/',
+  productionSourceMap: true
 })
