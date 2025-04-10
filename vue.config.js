9525292897210d863b3cf4cbeb550e6d0cad34d8
salemango <blackfootedcat2021@gmail.com>
2025-04-10 11:05:00 -0400

    public path change

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/character-trait-builder/'
+  publicPath: '/character-traits/'
 })
